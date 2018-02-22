6a7297d33db3e1743b0db5b9f34d83ef93f05932
Tyler Tran <tyml.tran@gmail.com>
2018-02-22 10:45:24 -0800

    Update lab7 to slide 45. This is experiment testing of the like button on two different pages.

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
 });
 
 $(".likeBtn").click(function(e) {
+	// Prevent page from reloading every time you click.
 	e.preventDefault();
 
-	// First, create a listener where Google Analytics can read it to.
+	// First, create a listener where Google Analytics can read it
+	// to. In this case, your User Analytics ID to your page.
 	ga('create', 'UA-114595572-1', 'auto');
 	// Google Analytics event:
 	// ga("send", "event", <Category>, <Action>)
