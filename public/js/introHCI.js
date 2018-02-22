d5a58e253b525225c7a49296d6808b15f6147312
Tyler Tran <tyml.tran@gmail.com>
2018-02-22 10:33:44 -0800

    update on lab7 up to slide 37

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
 	initializePage();
 });
 
+$(".likeBtn").click(function(e) {
+	e.preventDefault();
+
+	// First, create a listener where Google Analytics can read it to.
+	ga('create', 'UA-114595572-1', 'auto');
+	// Google Analytics event:
+	// ga("send", "event", <Category>, <Action>)
+	ga("send", "event", 'like', 'click');
+});
+
 /*
  * Function that is called when the document is ready.
  */
